bc94171d3d8c3354933d366a02db135e075c6a1f
Solee Lee <carolinesoleelee@gmail.com>
2018-10-04 20:21:02 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   answer = gets.chomp
   if answer == "exit"
     puts "Goodbye!"
-  elsif answer == this
+  elsif answer.to_i == this
     return puts "You guessed the correct number!"
     else answer != this
     return puts "The computer guessed #{this}."
